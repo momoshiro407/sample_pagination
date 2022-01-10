2ccecf9af15671ecf935215562a5ea14fcf86368
momoshiro407 <rozabmmsr@gmail.com>
2022-01-10 17:46:34 +0900

    ページネーション処理

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,13 +1,72 @@
 <template>
-  ListView
+  <div class="thumbnail-container">
+    <div class="thumbnail" v-for="(url, index) in displayImages" :key="index">
+      <img :src="url">
+    </div>
+  </div>
+  <div>
+    <Pagination
+      :total="dummyImages.length"
+      :imagesPerPage="imagesPerPage"
+      :displayRange="displayRange"
+      @currentPage="updateCurrentPage($event)"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import { ref } from 'vue'
+import { computed, defineComponent } from '@vue/runtime-core'
+import Pagination from '@/components/Pagination.vue'
 
-}
+export default defineComponent({
+  components: { Pagination },
+  setup() {
+    // 確認用ダミー画像データ
+    const imageSet = [
+      require('@/assets/images/green.jpg'),
+      require('@/assets/images/red.jpg'),
+      require('@/assets/images/blue.jpg'),
+    ]
+    // 三種類の画像をランダムに選んで配列に格納する
+    const dummyImages = new Array(103).fill(null).map(() => imageSet[Math.floor(Math.random() * 3)])
+    
+    // ページネーション関係のパラメータ
+    const currentPage = ref(1)
+    const imagesPerPage = 10
+    const displayRange = 9
+
+    const updateCurrentPage = (page: number) => {
+        currentPage.value = page
+    }
+
+    // 現在のページに表示する画像だけを抽出する
+    const displayImages = computed(() => {
+      const startIndex = (currentPage.value - 1) * imagesPerPage
+      const endIndex = startIndex + imagesPerPage
+      return dummyImages.slice(startIndex, endIndex)
+    })
+    
+    return { dummyImages, currentPage, imagesPerPage, displayRange, updateCurrentPage, displayImages }
+  }
+})
 </script>
 
-<style>
-
+<style scoped>
+  .thumbnail-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .thumbnail {
+    display: flex;
+    margin-top: 20px;
+    width: 200px;
+    height: 200px;
+    align-items: center;
+    justify-content: center;
+  }
+  img {
+    max-width: 195px;
+    max-height: 195px;
+  }
 </style>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,13 +1,124 @@
 <template>
-  Pagination
+  <div class="pagination-container">
+    <ul class="pagination">
+      <li v-if="!isLowerEnd" class="arrow" @click.prevent="setCurrentPage(1)">&lt;&lt;</li>
+      <li v-if="!isLowerEnd" class="arrow" @click.prevent="setCurrentPage(currentPage - 1)">&lt;</li>
+      <li v-for="page in displayPages"
+        :key="page"
+        @click.prevent="setCurrentPage(page)"
+        :class="page === currentPage ? 'active' : 'inactive'"
+      >
+        {{ page }}
+      </li>
+      <li v-if="!isUpperEnd" class="arrow" @click.prevent="setCurrentPage(currentPage + 1)">&gt;</li>
+      <li v-if="!isUpperEnd" class="arrow" @click.prevent="setCurrentPage(pages.length)">&gt;&gt;</li>
+    </ul>
+  </div>
+  <div>
+    {{ displayInfo }}
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import { ref } from 'vue'
+import { computed, defineComponent } from "@vue/runtime-core"
 
-}
+export default defineComponent ({
+  props: {
+    total: {
+      required: true,
+      type: Number
+    },
+    imagesPerPage: {
+      required: true,
+      type: Number
+    },
+    displayRange: {
+      required: true,
+      type: Number
+    }
+  },
+  emits: ['currentPage'],
+  setup(props, context) {
+    // 1から始まるページ番号を要素として持つ配列を作成する
+    const pages = new Array(Math.ceil(props.total / props.imagesPerPage)).fill(null).map((e, i) => i + 1)
+    const currentPage = ref(1)
+
+    // ページ送りボタン（<<, <, >, >>）の表示制御フラグ
+    const isLowerEnd = computed(() => {
+      return currentPage.value === 1
+    })
+    const isUpperEnd = computed(() => {
+      return currentPage.value === pages.length
+    })
+    
+    // ページ番号ボタンの表示制御
+    const displayPages = computed(() => {
+      // 最大でdisplayRange個のページ番号ボタンを表示する
+      if (pages.length > props.displayRange) {
+        let lowerIndex = currentPage.value - Math.floor(props.displayRange / 2) - 1
+        let upperIndex = currentPage.value + Math.floor(props.displayRange / 2) - 1
+        if (lowerIndex < 0) {
+          lowerIndex = 0
+        }
+        if (upperIndex > pages.length - 1) {
+          lowerIndex = pages.length - props.displayRange
+        }
+        return pages.slice(0, pages.length).splice(lowerIndex, props.displayRange)
+      } else {
+        return pages
+      }
+    })
+
+    const displayInfo = computed(() => {
+      const start = (currentPage.value - 1) * props.imagesPerPage + 1
+      const end = Math.min(currentPage.value * props.imagesPerPage, props.total)
+      
+      return `${ props.total }件中 ${ start } - ${ end }件を表示`
+    })
+
+    // 選択中のページ番号でcurrentPageを更新し、親コンポーネントに選択ページ番号の情報を送る
+    const setCurrentPage = (page: number) => {
+      if (page >= 1 && page <= pages.length ) {
+        currentPage.value = page
+        context.emit('currentPage', page)
+      }
+    }
+
+    return { pages, currentPage, isLowerEnd, isUpperEnd, displayPages, displayInfo, setCurrentPage }
+  }
+
+})
+
 </script>
 
-<style>
-
+<style scoped>
+  .pagination-container {
+    display: flex;
+    margin: 10px 0;
+    justify-content: center;
+  }
+  .pagination {
+    display: flex;
+    list-style-type: none;
+  }
+  li {
+    margin: 0 1px;
+    box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
+    border-radius: 3px;
+    padding: 6px 12px;
+    text-align: center;
+    cursor: pointer;
+  }
+  .active {
+    font-weight: bold;
+    color: #ffffff;
+    background: #5abb6a;
+  }
+  .inactive, .arrow {
+    background: #ffffff;
+  }
+  .inactive:hover, .arrow:hover  {
+    background: #c6e6cb;
+  }
 </style>
